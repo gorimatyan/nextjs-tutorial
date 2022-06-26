7737753cd5fc6598387f109c331c5a12534a2338
gorimatyan <gorima102131@gmail.com>
2022-06-26 20:28:01 +0900

    SSRのお試し

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -5,12 +5,51 @@ import styles from '../../styles/Home.module.css'
 
 // SSGは静的なページ、SSRは動的なページに向いているっぽい
 // SSG = getStaticProps(+getStaticPaths)
-// SSr = getServerSideProps  を使えばよいっぽい
+// SSR = getServerSideProps  を使えばよいっぽい
 
 
-// SSGの場合:手順は二つ
-// 1、getStaticPropsでpropsにfetchで取り出したデータを送る
-export const getStaticProps = async ({params}:any) => { // paramsには自動的にURLパラメータの値が入るっぽい
+// // SSGの場合:手順は二つ
+// // 1、getStaticPropsでpropsにfetchで取り出したデータを送る
+// export const getStaticProps = async ({params}:any) => { // paramsには自動的にURLパラメータの値が入るっぽい
+//     const req = await fetch(`http://localhost:3000/${params.id}.json`)
+//     const data = await req.json();
+    
+//     return {
+//         // コンポーネントにpropsの形で渡される。
+//         props: {
+//             product: data,
+//         }, 
+//       }
+// }
+// type product = {
+//     id: string,
+//     name: string,
+//     image: string,
+// }
+// // 2、ダイナミックルーティングを実装するためにgetStaticPathsを使ってパスを取得する
+// //「ダイナミックルーティングを実装する」というのは、「複数のURLに対応したページファイルを作る」ということと同じ意味です。
+// // 例えば home/products/[id] ←これ
+// // getStaticPathsを使って[id]に入る値を定義する必要がある。
+// export const getStaticPaths = async () => {
+//     const req = await fetch(`http://localhost:3000/products.json`)
+//     const data = await req.json();
+//     // map関数で["smartPhone", "pc", "headPhone"]を取り出す
+//     const paths = data.map(( product: string )=> {
+//         return {
+//             params: {
+//                 id: product,
+//             }, 
+//           }
+//     })
+    
+//     return {
+//         paths,
+//         fallback: false, // pathsに設定されていないURLはすべて404になる
+//     }
+// }
+
+// SSRの場合(ユーザーがrequestしたときにレンダリングするらしい)
+export const getServerSideProps = async ({params}:any) => { // paramsには自動的にURLパラメータの値が入るっぽい
     const req = await fetch(`http://localhost:3000/${params.id}.json`)
     const data = await req.json();
     
@@ -21,32 +60,9 @@ export const getStaticProps = async ({params}:any) => { // paramsには自動的
         }, 
       }
 }
-type product = {
-    id: string,
-    name: string,
-    image: string,
-}
-// 2、ダイナミックルーティングを実装するためにgetStaticPathsを使ってパスを取得する
-//「ダイナミックルーティングを実装する」というのは、「複数のURLに対応したページファイルを作る」ということと同じ意味です。
-// 例えば home/products/[id] ←これ
-// getStaticPathsを使って[id]に入る値を定義する必要がある。
-export const getStaticPaths = async () => {
-    const req = await fetch(`http://localhost:3000/products.json`)
-    const data = await req.json();
-    // map関数で["smartPhone", "pc", "headPhone"]を取り出す
-    const paths = data.map(( product: string )=> {
-        return {
-            params: {
-                id: product,
-            }, 
-          }
-    })
-    
-    return {
-        paths,
-        fallback: false, // pathsに設定されていないURLはすべて404になる
-    }
-}
+
+
+
 
 type Props = {
     product: {
@@ -55,7 +71,6 @@ type Props = {
         image: string,
     }
 }
-
 const Product: NextPage<Props> = (props: Props) => {
     const router = useRouter();
     const { id } = router.query;
